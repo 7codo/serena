5fa5064fe44ce16e4ef089b04e2a521b3f5998fe
7codo <ayoub.7codo@gmail.com>
2026-05-30 17:39:54 +0100

    refactor: comment out TypeScript language server cases in Language class for clarity

--- src/solidlsp/ls_config.py
+++ src/solidlsp/ls_config.py
@@ -279,11 +279,11 @@ class Language(str, Enum):
                 from solidlsp.language_servers.omnisharp import OmniSharp
 
                 return OmniSharp
-            case self.TYPESCRIPT:
-                from solidlsp.language_servers.typescript_language_server import TypeScriptLanguageServer
+            # case self.TYPESCRIPT:
+            #     from solidlsp.language_servers.typescript_language_server import TypeScriptLanguageServer
 
-                return TypeScriptLanguageServer
-            case self.TYPESCRIPT_VTS:
+            #     return TypeScriptLanguageServer
+            case self.TYPESCRIPT | self.TYPESCRIPT_VTS:
                 from solidlsp.language_servers.vts_language_server import VtsLanguageServer
 
                 return VtsLanguageServer
